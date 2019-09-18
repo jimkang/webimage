ae6f0649b44c788b85fec704cad7e5ffc7d442b5
Jim Kang <jimkang@gmail.com>
2019-09-17 21:21:59 -0400

    Fix animated gifs so that they show up on mobile phones and also Ubuntu Image Viewer. Swap omggif out for gif.js.

--- raw-images-to-animated-gif.js
+++ raw-images-to-animated-gif.js
@@ -1,12 +1,7 @@
 /* global Buffer */
-var callNextTick = require('call-next-tick');
-//var palette = require('get-rgba-palette');
-var omggif = require('omggif');
-var indexImage = require('./index-image');
 
-// TODO: Figure out how large that stuff actually is.
-const gifHeaderFooterSize = 1000;
-const requiredPaletteSize = 256;
+var callNextTick = require('call-next-tick');
+var { GIFEncoder } = require('gif.js');
 
 // Assumes images are all the same size. images should be Jimp image instances.
 function rawImagesToAnimatedGif({ images, frameLengthMS = 1000 / 30 }, done) {
@@ -18,40 +13,25 @@ function rawImagesToAnimatedGif({ images, frameLengthMS = 1000 / 30 }, done) {
     return;
   }
   var firstImage = images[0];
-  // Dividing by 4 here because there will be one index replacing the rgba values.
-  const totalImagesSize =
-    images.length *
-    (firstImage.bitmap.data.length / 4 +
-      gifHeaderFooterSize +
-      requiredPaletteSize);
-  var gifImage = new Buffer(totalImagesSize);
-  var gifWriter = new omggif.GifWriter(
-    gifImage,
+
+  var encoder = new GIFEncoder(
     firstImage.bitmap.width,
-    firstImage.bitmap.height,
-    { loop: 0 }
+    firstImage.bitmap.height
   );
+
+  encoder.writeHeader();
   images.forEach(addToGif);
-  const gifBufferSize = gifWriter.end();
-  callNextTick(done, null, gifImage.slice(0, gifBufferSize));
+  encoder.finish();
+
+  var pages = encoder.stream().pages;
+  var buffer = Buffer.concat(pages);
+
+  callNextTick(done, null, buffer);
 
   function addToGif(image) {
-    let { indexes, palette } = indexImage({ pixels: image.bitmap.data });
-    if (palette.length > requiredPaletteSize) {
-      console.error(
-        'Warning: Need to truncate palette from',
-        palette.length,
-        'colors down to',
-        requiredPaletteSize,
-        '.'
-      );
-      palette.splice(requiredPaletteSize);
-    }
-    // Delay unit is hundredths of second.
-    gifWriter.addFrame(0, 0, image.width, image.height, indexes, {
-      palette,
-      delay: frameLengthMS / 10
-    });
+    encoder.setRepeat(0);
+    encoder.setDelay(frameLengthMS / 10);
+    encoder.addFrame(image.bitmap.data);
   }
 }
 
